3edb1426abbbc1abd6f639785267995154381b19
Alexander Zapotylok <al3xzap@gmail.com>
2020-03-24 03:48:53 +0300

    изменение стартовой страницы

--- EducationHelper/Startup.cs
+++ EducationHelper/Startup.cs
@@ -58,23 +58,24 @@ namespace EducationHelper
                     });
             services.AddControllers();
             services.AddControllersWithViews();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
-
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseRouting();
+
 
             app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+               endpoints.MapControllers();
             });
         }
     }
